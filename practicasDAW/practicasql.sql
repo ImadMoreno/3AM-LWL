f1961adb1c9140eb5da236d211b3123e6b7c719a
Paula <paulacruz@iesgregorioprieto.com>
2024-01-30 23:54:42 +0100

    estructuras de carpetas DAW

--- practicasDAW/practicasql.sql
+++
@@ -1,5 +0,0 @@
-Connect system@xepdb1
-Drop user instituto cascade;
-Create user instituto identified by 123 quota unlimited on system;
-Grant connect, resource to instituto;
-Connect instituto/123@xepdb1;
